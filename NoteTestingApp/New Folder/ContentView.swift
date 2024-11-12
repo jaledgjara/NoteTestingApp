8e49e505c8afd0d1c1b78bb2f0e5a15b0d0a0669
Jaled Jara <jaledjara123@outlook.com>
2024-11-12 13:30:59 -0300

    Initial commit

---
+++ NoteTestingApp/New Folder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NoteTestingApp
+//
+//  Created by Jaled Jara on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteTestingApp/New Folder/NoteTestingAppApp.swift
@@ -0,0 +1,28 @@
+//
+//  NoteTestingAppApp.swift
+//  NoteTestingApp
+//
+//  Created by Jaled Jara on 11/12/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct NoteTestingAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
