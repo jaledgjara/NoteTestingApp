3d16d24aeb94d7d544e11e9ee54f582b0c3cc14d
Jaled Jara <jaledjara123@outlook.com>
2024-11-12 13:17:59 -0300

    Initial Commit

---
+++ NoteTestingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteTestingApp
+//
+//  Created by Jaled Jara on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteTestingApp/NoteTestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTestingAppApp.swift
+//  NoteTestingApp
+//
+//  Created by Jaled Jara on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteTestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
